0e1d96c87b0644b34dc954c0b7fb4dff6f263a93
ffasitella <federico.fasitella@wolox.com.ar>
2020-07-13 13:55:11 -0300

    Complete news view

--- app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
@@ -1,11 +1,9 @@
 package ar.com.wolox.android.example.ui.home
 
-import androidx.viewpager.widget.ViewPager
 import ar.com.wolox.android.R
 import ar.com.wolox.android.example.ui.news.NewsFragment
 import ar.com.wolox.android.example.ui.profile.ProfileFragment
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
-import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_home.*
 import javax.inject.Inject
 
@@ -17,9 +15,6 @@ class HomeFragment : WolmoFragment<HomePresenter>(), HomeView {
     @Inject
     lateinit var profileFragment: ProfileFragment
 
-    private lateinit var viewPager: ViewPager
-    private lateinit var tabLayout: TabLayout
-
     override fun layout() = R.layout.fragment_home
 
     override fun init() {
@@ -27,7 +22,7 @@ class HomeFragment : WolmoFragment<HomePresenter>(), HomeView {
         adapter.addFragment(newsFragment)
         adapter.addFragment(profileFragment)
         vHomeViewPager.adapter = adapter
-        vTabLayout.setupWithViewPager(viewPager)
+        vTabLayout.setupWithViewPager(vHomeViewPager)
     }
 
     companion object {
