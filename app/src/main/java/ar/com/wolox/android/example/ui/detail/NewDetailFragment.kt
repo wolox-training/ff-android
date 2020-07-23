051941bf97fcc820bb23a035c2031f7b329f611d
ffasitella <federico.fasitella@wolox.com.ar>
2020-07-23 12:44:40 -0300

    Placeholder view

---
+++ app/src/main/java/ar/com/wolox/android/example/ui/detail/NewDetailFragment.kt
@@ -0,0 +1,12 @@
+package ar.com.wolox.android.example.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ar.com.wolox.wolmo.core.fragment.WolmoFragment
+
+class NewDetailFragment : WolmoFragment() {
+
+}
